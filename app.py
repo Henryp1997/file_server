4f655013a41987310a5c66fe9f4986064e88d920
Henry.Pickersgill <henry.pickersgill@forefrontrf.com>
2025-05-22 17:32:43 +0100

    full functional but maybe a bit clunky

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash
 from dash import html, dcc, ctx
 from dash.dependencies import Input, Output, State, ALL
+import yaml
 import flask
 import os
 
@@ -10,47 +11,99 @@ import helper
 app = dash.Dash(__name__)
 server = app.server  # for hosting
 
-ROOT_DIR = r"C:\ALL_WORK\code\python-bi"
+with open("projects.yaml", "r") as f:
+    projects = yaml.safe_load(f)
 
 app.layout = html.Div([
     html.Div(
         html.H1(
             "GitLab File Server",
             className="title",
-            style={
-                "display": "inline-block",
-                "margin-right": "8vw",
-                "padding-top": "0",
-                "margin-top": "0",
-                "padding-left": "0"
-            }
         ),
         style={
-            "background-color": "#EFEFEF",
+            "background-color": "#FFE0E0",
+            "border-radius": "5px",
             "display": "flex",
             "justifyContent": "center",
             "alignItems": "center"
         }
     ),
-    html.H3("File Explorer", style={"font-size": "24px", "margin-left": "1.5vw"}),
-    dcc.Store(id="opened-folders", data=[]),
-    html.Ul(id="file-tree")
+    html.Div([
+        html.A(
+            "GitLab Projects",
+            id="header",
+            className="header_clickable",
+        ),
+        html.Div([
+            html.Ul(
+                [html.Li(
+                    html.A(
+                        project,
+                        id={"type": "project_link", "name": project, "path": path},
+                        className="htmlA_clickable",
+                        style={"font-size": "30px"},
+                        n_clicks=0
+                    ),
+                    style={"margin-bottom": "1.5vh"}
+                )
+                for project, path in projects.items()
+                ], 
+                id="project_list",
+                style={"listStyleType": "none", "display": "block"}
+            )
+        ]),
+        dcc.Store(id="current_project_path", data=None),
+        dcc.Store(id="opened-folders", data=[]),
+        html.Ul(id="file-tree", style={"display": "none"})
+    ], style={"background-color": "#F2F2FD"})
 ])
 
 
 ### CALLBACKS
-@app.server.route("/download/<path:filename>")
-def download_file(filename):
-    return flask.send_from_directory(ROOT_DIR, filename, as_attachment=True)
+# @app.server.route("/download/<path:filename>")
+# def download_file(filename):
+#     return flask.send_from_directory(ROOT_DIR, filename, as_attachment=True)
+
+
+@app.callback(
+    Output("file-tree", "style", allow_duplicate=True),
+    Output("project_list", "style", allow_duplicate=True),
+    Output("header", "children", allow_duplicate=True),
+    Output("current_project_path", "data"),
+    Input({"type": "project_link", "name": ALL, "path": ALL}, "n_clicks"),
+    prevent_initial_call=True
+)
+def choose_project(n_clicks_list):   
+    project = ctx.triggered_id
+    return {"display": "block"}, {"display": "none"}, "File Explorer (click to go back)", project["path"]
+    
+
+@app.callback(
+    Output("file-tree", "style", allow_duplicate=True),
+    Output("project_list", "style", allow_duplicate=True),
+    Output("header", "children", allow_duplicate=True),
+    Output("current_project_path", "data", allow_duplicate=True),
+    Output("opened-folders", "data", allow_duplicate=True),
+    Input("header", "n_clicks"),
+    State("header", "children"),
+    prevent_initial_call=True
+)
+def navigate_with_header(n, header_text):
+    if header_text == "File Explorer (click to go back)":
+        return {"display": "none"}, {"listStyleType": "none", "display": "block"}, "GitLab Projects", None, []
 
 
 @app.callback(
     Output("file-tree", "children"),
-    Input("opened-folders", "data")
+    Input("opened-folders", "data"),
+    Input("current_project_path", "data"),
+    State("file-tree", "style"),
 )
-def update_tree(opened):
-    file_tree = helper.populate_file_tree(ROOT_DIR, opened)
-    return file_tree
+def update_tree(opened, path, style):
+    if style["display"] == "block":
+        file_tree = helper.populate_file_tree(path, opened)
+        return file_tree
+    return ""
 
 
 @app.callback(
@@ -59,8 +112,12 @@ def update_tree(opened):
     State("opened-folders", "data"),
     prevent_initial_call=True
 )
-def toggle_folder(n_clicks, opened):
+def toggle_folder(n_clicks_list, opened):
     triggered = ctx.triggered_id
+
+    if all(n_clicks == 0 for n_clicks in n_clicks_list):
+        return opened
+    
     path = triggered["path"]
     if path in opened:
         opened.remove(path)
--- helper.py
+++ helper.py
@@ -8,22 +8,31 @@ def populate_file_tree(dir, opened_paths):
     root = list(base.glob("*"))
     files = []
     dirs = []
-
+    gitignore_list = get_gitignore_list(base)
     extensions = {
         ".txt" : "\U0001F4C4",
         ".yaml": "\u2699\uFE0F ",
         ".json": "\u2699\uFE0F ",
         ".py"  : "\U0001F40D",
-        ".csv" : "\U0001F4CA"
+        ".csv" : "\U0001F4CA",
+        ".m"   : "\U0001F4DD",
+        ".css" : "\U0001F4DD",
+        ".cpp" : "\U0001F4DD",
+        ".png" : "\U0001F5BC\uFE0F ",
+        ".jpg" : "\U0001F5BC\uFE0F ",
+        ".jpeg" : "\U0001F5BC\uFE0F "
     }
 
     root_files = [i for i in root if not i.is_dir()]
     root_dirs = list(set(root) - set(root_files))
 
     for file in root_files:
-        icon = extensions.get(file.suffix, extensions[".txt"])
-        name = file.name
-        files.append(create_list_item(icon, name, item_type="file", path=str(file)))
+        icon = extensions.get(file.suffix.lower(), extensions[".txt"])
+        file_name = file.name
+
+        do_append = check_if_ignored(file_name, "file", gitignore_list)
+        if do_append:
+            files.append(create_list_item(icon, file_name, item_type="file", path=str(file)))
 
     # Sort the file list. Priorities python then csv files
     # All other files sorted alphabetically
@@ -32,25 +41,69 @@ def populate_file_tree(dir, opened_paths):
     # Directories. Must be recursive
     for dir in root_dirs:
         icon = "\U0001F4C1"
-        name = dir.name
+        dir_name = dir.name
         path = str(dir)
-        is_open = path in opened_paths
-        child_items = populate_file_tree(path, opened_paths) if is_open else []
-        dirs.append(
-            create_list_item(
-                icon,
-                name,
-                item_type="dir",
-                path=path,
-                child_items=child_items,
-                ul_style={
-                    "marginLeft": "1em",
-                    "display": "block" if is_open else "none"
-                }
+
+        do_append = check_if_ignored(dir_name, "dir", gitignore_list)
+        if do_append:
+            is_open = path in opened_paths
+            child_items = populate_file_tree(path, opened_paths) if is_open else []
+            dirs.append(
+                create_list_item(
+                    icon,
+                    dir_name,
+                    item_type="dir",
+                    path=path,
+                    child_items=child_items,
+                    ul_style={
+                        "marginLeft": "1em",
+                        "display": "block" if is_open else "none"
+                    }
+                )
             )
-        )
 
     return dirs + files
+
+
+def get_gitignore_list(base):
+    """ First check if there is a .gitignore - don't want to show those files/dirs """
+    gitignore_list = []
+    if os.path.exists(base / ".gitignore"):
+        with open(base / ".gitignore", "r") as f:
+            gitignore_list = f.readlines()
+        gitignore_list = [i.strip("\n") for i in gitignore_list]
+    return gitignore_list
+
+
+def check_if_ignored(name, item_type, gitignore_list):
+    """ Check if a file/dir is in .gitignore so we can skip past it in app display """
+    do_append = True
+
+    # Check for fullstop in front of file/dir (hidden file/dir)
+    if name.startswith("."):
+        return False
+
+    for ignore in gitignore_list:
+        if name in ignore:
+            do_append = False
+
+        if item_type == "file":
+            if ignore == f"*{Path(name).suffix}":
+                # E.g., if *.csv in .gitignore, all .csv files are ignored
+                return False
+
+    # # Now check if files are actually a part of the git repo
+    # import subprocess
+
+    # repo_path = r"C:\ALL_WORK\code\python-bi"
+    # stderr = subprocess.run(
+    #     ["git", "ls-files", "--error-unmatch", name],
+    #     cwd=repo_path,
+    #     stderr=subprocess.PIPE
+    # )
+    # print(stderr)
+
+    return do_append
 
 
 def sort_priority(file_component: html.Li):
@@ -58,11 +111,10 @@ def sort_priority(file_component: html.Li):
     filename = Path(file_component.children[0].children)
     name = filename.name
     ext = filename.suffix
-    is_hidden = name.split(" ")[1].startswith(".")
 
     if ext == ".py": group = 0
-    elif ext == ".csv": group = 1
-    elif is_hidden: group = 2
+    elif ext == ".m": group = 1
+    elif ext == ".csv": group = 2
     else: group = 3
 
     return (group, name.lower())
